dce06a91254fdf090726603689fc6a8d20081a30
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-04 20:13:53 +0300

    shapes

---
+++ SwiftUI-Design-Course/Paths/PathsView.swift
@@ -0,0 +1,18 @@
+//
+//  PathsView.swift
+//  SwiftUI-Design-Course
+//
+//  Created by Omer Cagri Sayir on 4.02.2024.
+//
+
+import SwiftUI
+
+struct PathsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PathsView()
+}
--- SwiftUI-Design-Course/Shapes/ShapesView.swift
+++ SwiftUI-Design-Course/Shapes/ShapesView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ShapesView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Circle - It takes all the spaces available
+            Circle()
+                .frame(width: 100)
+                .foregroundStyle(.blue)
+
+            Ellipse()
+                .frame(width: 100, height: 60)
+
+            Rectangle()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+
+            RoundedRectangle(cornerRadius: 15)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+
+            // Commonly used for buttons
+            Capsule()
+                .foregroundStyle(.red)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 40)
         }
-        .padding()
     }
 }
 
