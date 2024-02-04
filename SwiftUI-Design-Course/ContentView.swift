124c9a16b713e7d98c9b4e7d940503f004b27a60
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-04 19:53:15 +0300

    Initial Commit

---
+++ SwiftUI-Design-Course/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Design-Course
+//
+//  Created by Omer Cagri Sayir on 4.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Design-Course/SwiftUI_Design_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Design_CourseApp.swift
+//  SwiftUI-Design-Course
+//
+//  Created by Omer Cagri Sayir on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Design_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
