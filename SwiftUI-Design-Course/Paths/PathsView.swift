1486c0393d2cfbfa3e16427e00c1e4b2d0d09cc3
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-05 15:31:49 +0300

    paths

--- SwiftUI-Design-Course/Paths/PathsView.swift
+++ SwiftUI-Design-Course/Paths/PathsView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct PathsView: View {
+    let customPath: Path = {
+        Path({ path in
+
+            // Series of commands to draw your shape
+            // It is like your hand and pencil
+            // X normal, y opposite. Coordinate starts left top
+            path.move(to: CGPoint(x: 100, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 150))
+            path.addLine(to: CGPoint(x: 200, y: 150))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 100))
+            path.closeSubpath()
+
+            path.move(to: CGPoint(x: 300, y: 200))
+            path.addLines([CGPoint(x: 300, y: 400), CGPoint(x: 400, y: 400)])
+
+        })
+    }()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+//            Path({ path in
+//
+//                // Series of commands to draw your shape
+//                // It is like your hand and pencil
+//                // X normal, y opposite. Coordinate starts left top
+//                path.move(to: CGPoint(x: 100, y: 100))
+//                path.addLine(to: CGPoint(x: 100, y: 150))
+//                path.addLine(to: CGPoint(x: 200, y: 150))
+//                path.addLine(to: CGPoint(x: 200, y: 100))
+//                path.addLine(to: CGPoint(x: 100, y: 100))
+//                path.closeSubpath()
+//
+//                path.move(to: CGPoint(x: 300, y: 200))
+//                path.addLines([CGPoint(x: 300, y: 400), CGPoint(x: 400, y: 400)])
+//
+//            })
+            customPath
+                .stroke(lineWidth: 5)
+                .foregroundStyle(.red)
+                .rotationEffect(Angle(degrees: 40))
+
+            customPath
+        }
     }
 }
 
