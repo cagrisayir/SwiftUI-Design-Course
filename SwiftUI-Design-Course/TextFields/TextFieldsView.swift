ba5ff626cbcf7ffc2db4cd418f445ff430535ba1
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-08 11:37:36 +0300

    textfield init

---
+++ SwiftUI-Design-Course/TextFields/TextFieldsView.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldsView.swift
+//  SwiftUI-Design-Course
+//
+//  Created by Omer Cagri Sayir on 6.02.2024.
+//
+
+import SwiftUI
+
+struct TextFieldsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldsView()
+}
